f4b5848927e1327fc50bbfcf14ebba1b948f3113
mrshuhrat <shuhratadm1n@gmail.com>
2020-11-16 14:31:19 +0500

    change js some codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
 var elPokemonsWrapper = document.querySelector('.js-pokemons-wrapper');
 
-for (var i = 0; i < pokemons.length; i++) {
-  var elNewDiv = document.createElement('div');
-  elNewDiv.classList.add('pokemon');
+// for (var i = 0; i < pokemons.length; i++)
+pokemons.forEach(function (pokemon) {
+  
+  var elNewDiv = createElement('div', 'pokemon');
+  /* var elNewDiv = document.createElement('div');
+  elNewDiv.classList.add('pokemon'); */
 
-  var elNewImg = document.createElement('img');
-  elNewImg.classList.add('pokemon__img');
-  elNewImg.src = pokemons[i].img;
+  var elNewImg = createElement('img', 'pokemon__img');
+  elNewImg.src =pokemon.img;
+  /* var elNewImg = document.createElement('img');
+  elNewImg.classList.add('pokemon__img'); */
 
-  var elNewHeading = document.createElement('h3');
+  var elNewHeading = createElement('h3', 'pokemon__name', pokemon.name);
+  /* var elNewHeading = document.createElement('h3');
   elNewHeading.classList.add('pokemon__name');
-  elNewHeading.textContent = pokemons[i].name;
+  elNewHeading.textContent = pokemons[i].name; */
 
-  var elNewParagraph = document.createElement('p');
+  var elNewParagraph = createElement('P', 'pokemon__type', pokemon.type);
+  /* var elNewParagraph = document.createElement('p');
   elNewParagraph.classList.add('pokemon__type');
-  elNewParagraph.textContent = pokemons[i].type;
+  elNewParagraph.textContent = pokemons[i].type; */
 
   elPokemonsWrapper.appendChild(elNewDiv);
   elNewDiv.appendChild(elNewImg);
   elNewDiv.appendChild(elNewHeading);
   elNewDiv.appendChild(elNewParagraph);
-}
+})
+
+  
