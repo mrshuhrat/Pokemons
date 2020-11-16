72a21e349e4071b1f469de3349747dbcd92d8725
mrshuhrat <shuhratadm1n@gmail.com>
2020-11-16 16:39:54 +0500

    change some codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 var elPokemonsWrapper = document.querySelector('.js-pokemons-wrapper');
+var elButton = document.querySelector('.js-button');
+var form = document.querySelector('.form');
+var typeSelect = document.querySelector('.type');
 
-// for (var i = 0; i < pokemons.length; i++)
-pokemons.forEach(function (pokemon) {
+var createElementHtml = function (array) {
+  // for (var i = 0; i < pokemons.length; i++)
+array.forEach(function (pokemon) {
   
   var elNewDiv = createElement('div', 'pokemon');
   /* var elNewDiv = document.createElement('div');
@@ -17,15 +21,39 @@ pokemons.forEach(function (pokemon) {
   elNewHeading.classList.add('pokemon__name');
   elNewHeading.textContent = pokemons[i].name; */
 
-  var elNewParagraph = createElement('P', 'pokemon__type', pokemon.type);
+  var elNewParagraph = createElement('p', 'pokemon__type', pokemon.type);
   /* var elNewParagraph = document.createElement('p');
   elNewParagraph.classList.add('pokemon__type');
   elNewParagraph.textContent = pokemons[i].type; */
+
+  var elNewSpan = createElement('span', 'pokemon__type', pokemon.weaknesses);
 
   elPokemonsWrapper.appendChild(elNewDiv);
   elNewDiv.appendChild(elNewImg);
   elNewDiv.appendChild(elNewHeading);
   elNewDiv.appendChild(elNewParagraph);
+  elNewDiv.appendChild(elNewSpan);
 })
+}
 
+createElementHtml(pokemons);
+
+
+form.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  elPokemonsWrapper.innerHTML = ``;
+  if (typeSelect.value === 'all') {
+    createElementHtml(pokemons);
+    return;
+  }
+
+  var filteredPokemonsByType = pokemons.filter(function(pokemon) {
+    var pokemonTypesInLowecase = [];
+    pokemon.type.forEach(function(type) {
+      pokemonTypesInLowecase.push(type.toLowerCase());
+    });
+    return pokemonTypesInLowecase.includes(typeSelect.value);
+  });
   
+  createElementHtml(filteredPokemonsByType);
+});
